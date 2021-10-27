28d16a582a0ce00e496f6bef85ddbe5c190c0f0b
Dis <dis-3-3@protonmail.com>
2021-10-27 22:54:11 +0300

    Refactoring RepositoryBaseOnDrive

--- SkillUpREST.Entity/Repository/Base/RepositoryBaseOnDrive.cs
+++ SkillUpREST.Entity/Repository/Base/RepositoryBaseOnDrive.cs
@@ -1,68 +1,97 @@
 ﻿namespace SkillUpREST.Entity.Repository;
 
-
 using Newtonsoft.Json;
 using SkillUpREST.Repositories.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Collections.Generic;
 
 
 public class RepositoryBaseOnDrive<TEntity> : RepositoryBase<TEntity>, IRepositoryOnDrive<TEntity> where TEntity : IEntity
 {
-    private object sync = new object();
+    private Locker locker;
 
     public RepositoryBaseOnDrive(string repositoryPath)
     {
+        locker = new Locker(new object());
         Location = repositoryPath;
     }
 
     public virtual string Location { get; protected set; }
 
+    #region Public
     public override void Insert(TEntity entity)
     {
-        File.WriteAllText(PathForEntity(entity), JsonForEntity(entity));
+        locker.UnderLock(InsertInternal, entity);
     }
-    public override void InsertMany(IEnumerable<TEntity> items)
+    public override void InsertMany(IEnumerable<TEntity> entities)
     {
-        foreach (var item in items)
-        {
-            File.WriteAllText(PathForEntity(item), JsonForEntity(item));
-        }
+        locker.UnderLock(InsertManyInternal, entities);
     }
     public override void Delete(TEntity entity)
     {
-        File.Delete(PathForEntity(entity));
+        locker.UnderLock(DeleteInternal, entity);
     }
     public override void DeleteById(Guid id)
     {
-        File.Delete(PathForId(id));
+        locker.UnderLock(DeleteByIdInternal, id);
     }
     public override void DeleteMany(IEnumerable<TEntity> entities)
     {
-        foreach (var entity in entities)
-        {
-            File.Delete(PathForEntity(entity));
-        }
+        locker.UnderLock(DeleteManyInternal, entities);
     }
     public override TEntity Find(params Predicate<TEntity>[] requirements)
+    {
+        return locker.UnderLock(FindInternal, requirements);
+    }
+    public override IEnumerable<TEntity> FindMany(params Predicate<TEntity>[] requirements)
+    {
+        return locker.UnderLock(FindManyInternal, requirements);
+    }
+    #endregion
+
+    #region Internal
+    private void InsertInternal(TEntity entity)
+    {
+        Save(entity);
+    }
+    private void InsertManyInternal(IEnumerable<TEntity> entities)
+    {
+        foreach (var entity in entities)
+        {
+            Save(entity);
+        }
+    }
+    private void DeleteInternal(TEntity entity)
+    {
+        DeleteFile(entity);
+    }
+    private void DeleteByIdInternal(Guid id)
+    {
+        DeleteFile(id);
+    }
+    private void DeleteManyInternal(IEnumerable<TEntity> entities)
+    {
+        foreach (var entity in entities)
+        {
+            DeleteFile(entity);
+        }
+    }
+    private TEntity FindInternal(params Predicate<TEntity>[] requirements)
     {
         foreach (var filePath in Directory.GetFiles(Location))
         {
             TEntity entity = default;
 
-            lock (sync)
+            if (File.Exists(filePath))
             {
-                if (File.Exists(filePath))
-                {
-                    var json = File.ReadAllText(filePath);
-                    var tmpEntity = EntityFromJson(json);
+                var json = File.ReadAllText(filePath);
+                var tmpEntity = EntityFromJson(json);
 
-                    if (requirements.All(requirement => requirement(entity)))
-                    {
-                        entity = tmpEntity;
-                    }
+                if (requirements.All(requirement => requirement(entity)))
+                {
+                    entity = tmpEntity;
                 }
             }
 
@@ -76,23 +105,20 @@ public class RepositoryBaseOnDrive<TEntity> : RepositoryBase<TEntity>, IReposito
 
         return default;
     }
-    public override IEnumerable<TEntity> FindMany(params Predicate<TEntity>[] requirements)
+    private IEnumerable<TEntity> FindManyInternal(params Predicate<TEntity>[] requirements)
     {
         foreach (var filePath in Directory.GetFiles(Location))
         {
             TEntity entity = default;
 
-            lock (sync)
+            if (File.Exists(filePath))
             {
-                if (File.Exists(filePath))
-                {
-                    var json = File.ReadAllText(filePath);
-                    var tmpEntity = EntityFromJson(json);
+                var json = File.ReadAllText(filePath);
+                var tmpEntity = EntityFromJson(json);
 
-                    if (requirements.All(requirement => requirement(entity)))
-                    {
-                        entity = tmpEntity;
-                    }
+                if (requirements.All(requirement => requirement(entity)))
+                {
+                    entity = tmpEntity;
                 }
             }
 
@@ -104,9 +130,63 @@ public class RepositoryBaseOnDrive<TEntity> : RepositoryBase<TEntity>, IReposito
             yield return entity;
         }
     }
+    #endregion
 
-    string PathForId(Guid id) => Path.Combine(Location, id.ToString());
-    string PathForEntity(TEntity entity) => Path.Combine(Location, entity.Id.ToString());
-    TEntity EntityFromJson(string json) => JsonConvert.DeserializeObject<TEntity>(json);
-    string JsonForEntity(TEntity entity) => JsonConvert.SerializeObject(entity, Formatting.Indented);
+    private void Save(TEntity entity) => File.WriteAllText(path: PathForEntity(entity),
+                                                           contents: JsonForEntity(entity));
+    private void DeleteFile(TEntity entity) => File.Delete(path: PathForEntity(entity));
+    private void DeleteFile(Guid id) => File.Delete(path: PathForId(id));
+    private string PathForId(Guid id) => Path.Combine(Location, id.ToString());
+    private string PathForEntity(TEntity entity) => Path.Combine(Location, entity.Id.ToString());
+    private TEntity EntityFromJson(string json) => JsonConvert.DeserializeObject<TEntity>(json);
+    private string JsonForEntity(TEntity entity) => JsonConvert.SerializeObject(entity, Formatting.Indented);
+
+    private class Locker
+    {
+        private object sync;
+
+        public Locker(object sync)
+        {
+            this.sync = sync;
+        }
+
+        public TOut UnderLock<TOut>(Func<TOut> function)
+        {
+            TOut value = default;
+
+            lock (sync)
+            {
+                value = function();
+            }
+
+            return value;
+        }
+
+        public TOut UnderLock<TIn, TOut>(Func<TIn, TOut> function, TIn arg)
+        {
+            TOut value = default;
+
+            lock (sync)
+            {
+                value = function(arg);
+            }
+
+            return value;
+        }
+
+        public void UnderLock(Action action)
+        {
+            lock (sync)
+            {
+                action();
+            }
+        }
+        public void UnderLock<T>(Action<T> action, T arg)
+        {
+            lock (sync)
+            {
+                action(arg);
+            }
+        }
+    }
 }
